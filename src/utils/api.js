9e69f3919f2630d74654fc6648644ae19b100205
saikalyan2001 <52730178+saikalyan2001@users.noreply.github.com>
2025-06-15 16:29:08 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://attendance-management-backend-wgvu.onrender.com',
+  baseURL: 'https://attendance-management-backend-wgvu.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
